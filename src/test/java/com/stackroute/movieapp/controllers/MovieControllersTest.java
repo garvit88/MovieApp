b456a32abb14bc7390ae8eb4d4a54c76fdea6621
garvit88 <garvitgarg88@gmail.com>
2018-07-17 14:56:00 +0530

    testing test case

---
+++ src/test/java/com/stackroute/movieapp/controllers/MovieControllersTest.java
@@ -0,0 +1,14 @@
+package com.stackroute.movieapp.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MovieControllersTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
